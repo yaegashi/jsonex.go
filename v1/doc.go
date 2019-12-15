6235f461dc24f173337df215dea9c29f2f438240
YAEGASHI Takeshi <yaegashi@gmail.com>
2019-12-16 06:23:03 +0900

    Add doc.go

---
+++ v1/doc.go
@@ -0,0 +1,4 @@
+// Package json is a drop-in replacement of the original encoding/json.
+// It provides a catch-all capability for unknown fields using a special map tagged with `jsonex`.
+// See https://github.com/yaegashi/jsonex.go for details on this functionality.
+package json
